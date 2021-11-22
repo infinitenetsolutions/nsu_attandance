74737b4e054ff4b5deef0c41d1161fef9d6d90f8
raja <raja@raja.com>
2021-11-22 14:42:01 +0530

    first commit

---
+++ include/footer.php
@@ -0,0 +1,4 @@
+<footer class=" footer text-center mt-5 p-5">
+
+<strong><p>&copy; <?php echo date("Y"); ?> <a class="text-danger" target="_blank" href="https://www.nsuniv.in/">NETAJI SUBHAS UNIVERSITY </a> All Right Reserved. Powered By  <a class="text-success" href="http://infinitenetsolutions.com/" target="_blank">Infinite Net Solutions</a></p>
+</footer>
