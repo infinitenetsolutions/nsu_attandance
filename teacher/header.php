55f65b63ea6581f849664707c1e95d14450b76f3
raja <raja@raja.com>
2021-11-29 17:24:49 +0530

    done

--- teacher/header.php
+++ teacher/header.php
@@ -45,9 +45,9 @@ if(!isset($_SESSION["admin_id"]))
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
   <!-- <a class="navbar-brand" href="index.php">Home</a> -->
 
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+  <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
     <span class="navbar-toggler-icon"></span>
-  </button>
+  </button> -->
   <div class="collapse navbar-collapse" id="collapsibleNavbar">
     <ul class="navbar-nav">
       <!-- <li class="nav-item">
